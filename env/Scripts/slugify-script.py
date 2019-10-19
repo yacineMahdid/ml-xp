0c1e1e89564c10ed3259abff620c7d86d6f19277
Bl0101 <yacine.mahdid@live.com>
2019-10-18 22:35:44 -0400

    add titanic kaggle

---
+++ env/Scripts/slugify-script.py
@@ -0,0 +1,12 @@
+#!c:\users\biapt\documents\github\ml-xp\env\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'python-slugify==3.0.6','console_scripts','slugify'
+__requires__ = 'python-slugify==3.0.6'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('python-slugify==3.0.6', 'console_scripts', 'slugify')()
+    )
